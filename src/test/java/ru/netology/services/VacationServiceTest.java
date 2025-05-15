c5a965e5ea387ad39ef1b057d67f1c45faef2213
Mars <marosakraid@gmail.com>
2025-05-16 02:42:55 +0500

    second commit

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class VacationServiceTest {
